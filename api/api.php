f01bd13cce7fa8e92b1f096be659d08d2608a876
David Machado Monteiro <2211849@my.ipleiria.pt>
2021-12-14 09:40:43 -0100

    API corrections

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8');
-	header("Location:http://127.0.0.1/prsi/PHP_Server_Test/index.html");
+	//header("Location:http://127.0.0.1/prsi/PHP_Server_Test/index.html");
 	$type = $_SERVER["REQUEST_METHOD"];
 	if($type == "POST") {
 		 echo "Recebido um POST\n";
@@ -9,17 +9,16 @@
 			 echo "Valores aceites. \nGuardando em ficheiros locais.";
 			 $nome = $_POST["nome"];
 			 $date = str_replace("-","/",$_POST["data"])." ".$_POST["hora"];
-			 echo file_put_contents("../files/".$nome."/valor.txt",$_POST["valor"]."º");
+			 echo file_put_contents("../files/".$nome."/valor.txt",$_POST["valor"]);
 			 echo file_put_contents("../files/".$nome."/hora.txt", $date);
-			 echo file_put_contents("../files/".$nome."/log.txt", "\n".$date.";".$_POST["valor"]."º",FILE_APPEND);
+			 echo file_put_contents("../files/".$nome."/log.txt", "\n".$date.";".$_POST["valor"].($nome == "temperatura" ? "º" : "%"),FILE_APPEND);
 
 		 } else {
 			 echo "Falta de dados. \nLocal save cancelada.";
 		 }
 	} else if ($type == "GET") {
-		echo "Checking for data"
-		 	$data = file_get_contents("../files/".$_GET["nome"]."/log.txt");
-			//echo $data
+		$data = file_get_contents("../files/".$_GET["nome"]."/".$_GET["type"].".txt");
+		echo $data;
 	} else {
 		 echo "Metodo não permitido";
 	}
